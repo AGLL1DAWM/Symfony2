19e4de394d14033cd4979c88e8c392c045dfc345
alvaro <garcia20377@gmail.com>
2021-03-29 14:21:48 +0200

    Punto 12 completo

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -29,12 +29,14 @@ class ConferenceController extends AbstractController
         ]));
     }
 
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository): Response
     {
         $offset = max(0, $request->query->getInt('offset', 0));
+        
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         
         return new Response($this->twig->render('conference/show.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
