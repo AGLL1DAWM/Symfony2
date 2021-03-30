477d2e1251f907227aa040face5f38ba1761eb44
alvaro <garcia20377@gmail.com>
2021-03-30 10:13:16 +0200

    Punto16 Completo

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
+use App\SpamChecker;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -37,7 +38,7 @@ class ConferenceController extends AbstractController
         ]));
     }
 
-    public function show(Request $request, Conference $conference, ConferenceRepository $conferenceRepository , CommentRepository $commentRepository, string $photoDir): Response
+    public function show(Request $request, Conference $conference, ConferenceRepository $conferenceRepository , CommentRepository $commentRepository, SpamChecker $spamChecker, string $photoDir): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
@@ -56,6 +57,17 @@ class ConferenceController extends AbstractController
             }
 
             $this->entityManager->persist($comment);
+
+            $context = [
+                    'user_ip' => $request->getClientIp(),
+                    'user_agent' => $request->headers->get('user-agent'),
+                    'referrer' => $request->headers->get('referer'),
+                    'permalink' => $request->getUri(),
+                ];
+                if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                    throw new \RuntimeException('Blatant spam, go away!');
+                }
+
             $this->entityManager->flush();
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
