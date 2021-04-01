3a87d3f46b2b910a0a04271dd8688ef7616bd84a
alvaro <garcia20377@gmail.com>
2021-04-01 09:38:46 +0200

    Prueba gmail

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
+
+class MailerController extends AbstractController
+{
+    public function sendEmail(MailerInterface $mailer){
+        $email = (new Email())
+            ->from('auu298391@gmail.com')
+            ->to('auu298391@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+        
+            $mailer->send($email);
+
+            return new Response("Mensaje enviado",200);
+    }
+}
